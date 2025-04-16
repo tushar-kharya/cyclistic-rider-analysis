2e67f4539dc99728e382217005959d963596d01c
Tushar Kharya <tusharkharya@gmail.com>
2025-04-16 16:42:49 -0700

    Update rides_per_day_line_chart.py

--- DataVisualization/src/rides_per_day_line_chart.py
+++ DataVisualization/src/rides_per_day_line_chart.py
@@ -1,14 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Ensure file is read
 combined_df = pd.read_csv('202406-divvy-tripdata.csv')
 
-# Ensuring parsing of the date columns
 combined_df['started_at'] = pd.to_datetime(combined_df['started_at'])
 combined_df['day_of_week'] = combined_df['started_at'].dt.dayofweek + 1
 
-# Grouping the variables and plotting line chart
 day_of_week_counts = combined_df.groupby(['day_of_week', 'member_casual']).size().unstack()
 day_of_week_counts.plot(kind='line', marker='o')
 
